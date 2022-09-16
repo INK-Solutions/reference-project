1e8d2f5cf15fcdd3b2a07843a33725ca8f01d6c4
amerdani <amerdani@seedz.tech>
2022-09-16 18:01:56 +0200

    Add a template project

---
+++ src/main/java/tech/seedz/lostemplateproject/LosTemplateProjectApplication.java
@@ -0,0 +1,13 @@
+package tech.seedz.lostemplateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosTemplateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosTemplateProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/seedz/lostemplateproject/LosTemplateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.seedz.lostemplateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LosTemplateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
