5cff4507d080577bd6353b7b7a8bc2e576ca1626
amerdani <amerdani@seedz.tech>
2022-09-18 10:38:29 +0200

    Update los template project

--- src/test/java/tech/seedz/lostemplateproject/systemtest/LosSystemTest.java
+++ src/test/java/tech/seedz/lostemplateproject/systemtest/LosSystemTest.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.sql.DataSource;
 
 public class LosSystemTest extends AbstractSystemTest {
-    private static final String HAPPY_FLOW_SCENARIO_PATH = "tech/seedz/los/systemtest/scenarios/happy-flow/";
+    //private static final String HAPPY_FLOW_SCENARIO_PATH = "tech/seedz/los/systemtest/scenarios/happy-flow/";
 
     @Autowired
     private DataSource dataSource;
